27fbd4d090045835a0e8398386de8b4d32a286fc
Alex Karnovsky <alex@dralex.com>
2019-02-02 15:37:54 +0200

    Find all links and displays the text

--- test_almtoolbox_gitlab.py
+++ test_almtoolbox_gitlab.py
@@ -10,11 +10,17 @@ class PythonOrgSearch(unittest.TestCase):
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("http://almtoolbox.com/gitlab.php")
-        self.assertIn("GitLab", driver.title)
-        elem = driver.find_element_by_id("gsc-i-id1")
-        elem.send_keys("software")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+        #self.assertIn("GitLab", driver.title)
+        #elem = driver.find_element_by_id("gsc-i-id1")
+        #elem.send_keys("software")
+        #elem.send_keys(Keys.RETURN)
+        #assert "No results found." not in driver.page_source
+
+        # Check all links
+        links = driver.find_elements_by_tag_name("a")
+        for elem in links:
+            print ("elem = ", elem.get_attribute('textContent'))
+
 
 
     def tearDown(self):
